dae24970e1859a0c5b6e189dd136a2e60dcc7a3e
bilegt <bilgee0629@gmail.com>
2015-04-08 17:18:38 +0800

    session

--- server.go
+++ server.go
@@ -1,5 +1,11 @@
 package server
 
+import (
+	"log"
+	"net"
+	"os"
+)
+
 type Server struct {
 	Addr     string // address of the server
 	Appname  string // application name. mentioned in HELO command etc.
@@ -16,13 +22,34 @@ func StartServer(addr string, appname, string, hostname string, port string) err
 }
 
 func (server *Server) Start() error {
-	if srv.Addr == "" {
-		srv.Addr = srv.Port
+	if server.Addr == "" {
+		server.Addr = server.Port
 	}
-	if srv.Appname == "" {
-		srv.Appname = "SMTPD SERVER"
+	if server.Appname == "" {
+		server.Appname = "SMTPD SERVER"
 	}
-	if srv.Hostname == "" {
-		srv.Hostname, _ = os.Hostname()
+	if server.Hostname == "" {
+		server.Hostname, _ = os.Hostname()
+	}
+	ln, err := net.Listen("tcp", server.Port)
+	if err != nil {
+		return err
+	}
+	return server.Serve(ln)
+}
+
+func (server *Server) Serve(ln net.Listener) error {
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("%s: Error has occured: %v", server.Appname, err)
+			continue
+		}
+		session, err := server.NewSession(conn)
+		if err != nil {
+			continue
+		}
+		go session.HandleConnection()
 	}
 }
---
+++ session.go
@@ -0,0 +1,76 @@
+package session
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+type session struct {
+	server     *Server
+	conn       net.Conn
+	remoteIP   string
+	remoteHost string
+	remoteName string
+}
+
+func (server *Server) NewSession(conn net.Conn) (s *session, err error) {
+	s = &session{
+		server: server,
+		conn:   conn,
+	}
+	return
+}
+
+func (s *session) HandleConnection() {
+	defer s.conn.Close()
+	var from string
+	var to []string
+	buf := make([]byte, 4096)
+
+	s.remoteIP, _, _ = net.SplitHostPort(s.conn.RemoteAddr().String())
+	names, err := net.LookupAddr(s.remoteIP)
+	if err == nil && len(names) > 0 {
+		s.remoteHost = names[0]
+	} else {
+		s.remoteHost = "unknown"
+	}
+
+	//greet here
+	_, err := s.conn.Write([]byte(fmt.Sprintf("220 %s %s SMTP service ready", s.server.Hostname, s.server.Appname)))
+	for {
+		n, err := c.Read(buf)
+
+		//Print client commands
+		fmt.Println(string(buf[0:n]))
+		if err != nil || n == 0 {
+			c.Close()
+			break
+		}
+		//exclude new line characters
+		line := string(buf[0 : n-2])
+		command, args := ParseCommand(line)
+
+		switch command {
+		case "EHLO", "HELO":
+			server.remoteName = args
+			//server.conn.
+			s.Writef("250 %s greets %s", s.server.Hostname, s.remoteName)
+		}
+	}
+}
+
+func ParseCommand(line string) (command string, args string) {
+	if i := strings.Index(line, " "); i != -1 {
+		command = strings.ToUpper(line[:i])
+		args = strings.TrimSpace(line[i+1 : len(line)])
+	} else {
+		verb = strings.ToUpper(line)
+		args = ""
+	}
+	return verb, args
+}
+
+func (s *session) Writef(format string, args ...interface{}) {
+	s.conn.Write([]byte(fmt.Sprintf(format, args...)))
+}
